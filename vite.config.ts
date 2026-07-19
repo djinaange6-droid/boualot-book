37b89a9f11432505ab5a75ae788f6c09f2ac225a
djina <djinaange6@gmail.com>
2026-07-19 14:05:17 +0200

    build: externalize html2pdf in vite config and uninstall local package

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  build: {
+    rollupOptions: {
+      // Dis explicitement à Vite de ne pas toucher ni analyser html2pdf.js
+      external: ['html2pdf.js'],
+    },
+  },
+})
